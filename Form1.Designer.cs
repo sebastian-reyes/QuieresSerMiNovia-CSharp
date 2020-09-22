a4dc29b372828c1967fe2b1c17ed697f38160d06
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2020-09-22 13:09:16 -0500

    version 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace QuieresSerMiNovia
+{
+    partial class uwu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(uwu));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblAyuda = new System.Windows.Forms.Label();
+            this.pbCorazon = new System.Windows.Forms.PictureBox();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnNO = new System.Windows.Forms.Button();
+            this.btnSi = new System.Windows.Forms.Button();
+            this.lblPregunta = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCorazon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.btnNO);
+            this.panel1.Controls.Add(this.btnSi);
+            this.panel1.Controls.Add(this.lblPregunta);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(443, 266);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblAyuda);
+            this.panel2.Controls.Add(this.pbCorazon);
+            this.panel2.Controls.Add(this.lblRespuesta);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(443, 266);
+            this.panel2.TabIndex = 4;
+            // 
+            // lblAyuda
+            // 
+            this.lblAyuda.AutoSize = true;
+            this.lblAyuda.BackColor = System.Drawing.Color.Transparent;
+            this.lblAyuda.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAyuda.Location = new System.Drawing.Point(331, 238);
+            this.lblAyuda.Name = "lblAyuda";
+            this.lblAyuda.Size = new System.Drawing.Size(109, 19);
+            this.lblAyuda.TabIndex = 3;
+            this.lblAyuda.Text = "Toca el corazón ";
+            // 
+            // pbCorazon
+            // 
+            this.pbCorazon.BackColor = System.Drawing.Color.Transparent;
+            this.pbCorazon.Image = global::QuieresSerMiNovia.Properties.Resources.pngocean_com;
+            this.pbCorazon.Location = new System.Drawing.Point(173, 146);
+            this.pbCorazon.Name = "pbCorazon";
+            this.pbCorazon.Size = new System.Drawing.Size(100, 90);
+            this.pbCorazon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbCorazon.TabIndex = 2;
+            this.pbCorazon.TabStop = false;
+            this.pbCorazon.Click += new System.EventHandler(this.pbCorazon_Click);
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.BackColor = System.Drawing.Color.Transparent;
+            this.lblRespuesta.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRespuesta.Location = new System.Drawing.Point(1, 72);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(446, 36);
+            this.lblRespuesta.TabIndex = 1;
+            this.lblRespuesta.Text = "Siempre supe que te gustaba :3";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::QuieresSerMiNovia.Properties.Resources.FondoRespuesta;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(443, 266);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnNO
+            // 
+            this.btnNO.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNO.Location = new System.Drawing.Point(249, 160);
+            this.btnNO.Name = "btnNO";
+            this.btnNO.Size = new System.Drawing.Size(65, 54);
+            this.btnNO.TabIndex = 3;
+            this.btnNO.Text = "NO";
+            this.btnNO.UseVisualStyleBackColor = true;
+            this.btnNO.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnNO_MouseMove);
+            // 
+            // btnSi
+            // 
+            this.btnSi.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSi.Location = new System.Drawing.Point(129, 160);
+            this.btnSi.Name = "btnSi";
+            this.btnSi.Size = new System.Drawing.Size(65, 54);
+            this.btnSi.TabIndex = 2;
+            this.btnSi.Text = "SÍ";
+            this.btnSi.UseVisualStyleBackColor = true;
+            this.btnSi.Click += new System.EventHandler(this.btnSi_Click);
+            // 
+            // lblPregunta
+            // 
+            this.lblPregunta.AutoSize = true;
+            this.lblPregunta.BackColor = System.Drawing.Color.Transparent;
+            this.lblPregunta.Font = new System.Drawing.Font("Microsoft YaHei", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPregunta.Location = new System.Drawing.Point(38, 72);
+            this.lblPregunta.Name = "lblPregunta";
+            this.lblPregunta.Size = new System.Drawing.Size(378, 42);
+            this.lblPregunta.TabIndex = 1;
+            this.lblPregunta.Text = "¿Quieres ser mi novia?";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QuieresSerMiNovia.Properties.Resources.FondoPregunta;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(443, 266);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // uwu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 266);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "uwu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "uwu";
+            this.Load += new System.EventHandler(this.uwu_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCorazon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnNO;
+        private System.Windows.Forms.Button btnSi;
+        private System.Windows.Forms.Label lblPregunta;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.PictureBox pbCorazon;
+        private System.Windows.Forms.Label lblAyuda;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuieresSerMiNovia
+{
+    public partial class uwu : Form
+    {
+        public uwu()
+        {
+            InitializeComponent();
+        }
+
+        private void uwu_Load(object sender, EventArgs e)
+        {
+            lblPregunta.Parent = pictureBox1;
+            lblRespuesta.Parent = pictureBox2;
+            pbCorazon.Parent = pictureBox2;
+            lblAyuda.Parent = pictureBox2;
+            btnSi.TabStop = false;
+            btnNO.TabStop = false;
+            panel2.Hide();
+        }
+
+        public void MoverBoton()
+        {
+            Random r = new Random();
+            int x = r.Next(0, this.Width - btnNO.Width);
+            int y = r.Next(0, this.Height - btnNO.Height);
+            btnNO.Location = new Point(x, y);
+        }
+
+        private void btnNO_MouseMove(object sender, MouseEventArgs e)
+        {
+            MoverBoton();
+            if (btnNO.Location == btnSi.Location || btnNO.Location == lblPregunta.Location)
+            {
+                MoverBoton();
+            }
+        }
+
+        private void btnSi_Click(object sender, EventArgs e)
+        {
+            panel2.Show();
+        }
+
+        private void pbCorazon_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
